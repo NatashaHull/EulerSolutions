54f4c384fbc7d7f846639fa784ef896b2ec062bb
Natasha Hull <musicalnat@gmail.com>
2014-01-14 22:56:09 -0800

    Made brute force solution twice as fast as subsets solution to problem 30

--- Problem30-alt.rb
+++ Problem30-alt.rb
@@ -1,10 +1,10 @@
-#Takes 3 seconds longer than subset method
 def is_digit_fifth_power?(num, arr)
   arr.inject(0) { |accum, el| accum += el ** 5 } == num
 end
 
 sum = 0
-(32..999999).each do |i|
+max = 9 ** 5 * 6
+(33..max).each do |i|
   i_arr = i.to_s.split('').map(&:to_i)
   sum += i if is_digit_fifth_power?(i, i_arr)
 end
